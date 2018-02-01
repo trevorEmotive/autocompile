86e67a6d2db5d5674dfc62ad7cd6f1e5386c8c41
trevor aukim <trevor@thinkemotive.com>
2018-02-01 18:06:32 +0000

    Initial Commit

---
+++ AutoBuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoBuild
+//
+//  Created by trevor aukim on 01/02/2018.
+//  Copyright © 2018 trevor aukim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
